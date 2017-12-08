7fb93ccc6a566146d51e3d9d959807bd11033124
Christoph Frick <cf@ofnir.net>
2017-12-08 21:25:06 +0100

    Update to Clojure 1.9.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject net.ofnir/wrepl.puget "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.9.0-beta4"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [mvxcvi/puget "1.0.2"]
                  [integrant "0.6.1"]])
