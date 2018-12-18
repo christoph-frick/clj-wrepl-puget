faa1952c4e437aebf674709bbdd442c098c44cdf
Christoph Frick <cf@ofnir.net>
2018-12-18 23:02:15 +0100

    Update versions

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject net.ofnir/wrepl.puget "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [mvxcvi/puget "1.0.2"]
-                 [integrant "0.6.1"]])
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [mvxcvi/puget "1.1.0"]
+                 [integrant "0.7.0"]])
