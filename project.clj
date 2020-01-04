66630751b6f53213b0d8b977f8e67a4781b0a249
Christoph Frick <cf@ofnir.net>
2020-01-04 13:54:10 +0100

    Update deps

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject net.ofnir/wrepl.puget "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [mvxcvi/puget "1.1.0"]
+                 [mvxcvi/puget "1.2.0"]
                  [integrant "0.7.0"]])
