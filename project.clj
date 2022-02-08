ab383f48150248e0fe6fbbbbf48a6e7ca1ec31ac
Christoph Frick <cf@ofnir.net>
2022-02-08 17:31:25 +0100

    Update versions; use explicit deploy repo

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject net.ofnir/wrepl.puget "0.1.1-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [mvxcvi/puget "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [mvxcvi/puget "1.3.2"]
                  [integrant "0.8.0"]]
-  :deploy-repositories [["releases" :clojars]])
+  :deploy-repositories {"releases" {:url "https://clojars.org/repo/" :creds :gpg}})
